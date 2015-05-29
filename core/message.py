4f82cf7a8821dd3aab0fff02f355c68d343320eb
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-05-29 18:14:22 +0200

    Added method to create a Message from a dict and overrided the __str__ method.

--- core/message.py
+++ core/message.py
@@ -2,6 +2,9 @@
 """ This script defines the message object used to represent
     a message to display on a LCD screen.
 """
+
+import copy
+
 class Message(object):
     """ This object is a message to display on a LCD screen.
     """
@@ -9,3 +12,16 @@ class Message(object):
         self.content = content
         self.sender = sender
         self.other_params = kwargs
+
+    def __str__(self):
+        """ Return a string representation of the message.
+        """
+        return "("+self.sender+":"+self.content+")"
+
+def create_message_from_dict(dictionnary):
+    copied_dict = copy.copy(dictionnary)
+    content = copied_dict['content']
+    sender = copied_dict['sender']
+    del(copied_dict['content'])
+    del(copied_dict['sender'])
+    return Message(content=content, sender=sender, kwargs=copied_dict)
