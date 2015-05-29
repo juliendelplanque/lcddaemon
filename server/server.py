3e8277c6c41d93b553dc36e04bf35d08b1855f35
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-05-29 18:16:20 +0200

    Added (a part of) queue management.

--- server/server.py
+++ server/server.py
@@ -7,17 +7,24 @@ import json
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 
+from core.message import *
+
+message_queue = None
+
 class MessageHandler(BaseHTTPRequestHandler):
     """ This object is the handler for messages.
         Messages are encoded as json.
     """
-    def do_POST(self): #TODO
+    def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        print(post_data)
         message_dict = json.loads(str(post_data, 'UTF-8'))
-        print(message_dict)
-
+        print("JSON received: "+str(message_dict))
+        m = create_message_from_dict(message_dict)
+        print("Message created: "+str(m))
+        message_queue.put(m)
+        print("Message is now in the queue.")
+        print(str(message_queue))
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
@@ -32,10 +39,9 @@ class MessageHandler(BaseHTTPRequestHandler):
     def error_response(self):
         return self.response(1, "Error, message not in the queue.")
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+def run(queue, server_class=HTTPServer, handler_class=MessageHandler):
+    global message_queue
+    message_queue = queue
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
-
-if __name__ == '__main__': # For debug purpose 
-    run(handler_class=MessageHandler)
