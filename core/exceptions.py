2af4d69b6b0c88565ef32f70ddec56c1fb61d8d3
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-05-30 22:20:48 +0200

    Added error codes as constants in the module and added a 'code' instance variable in ParametersException.

--- core/exceptions.py
+++ core/exceptions.py
@@ -2,17 +2,21 @@
 """ This script defines exceptions that can be raised by the applications.
 """
 
+BAD_PARAMETER_TYPE  = 1
+BAD_PARAMETER_VALUE = 2
+
 class ParametersException(Exception):
     """ This exception is raised when a parameter has a bade type or a bad
         value.
     """
-    def __init__(self, cause):
+    def __init__(self, cause, code):
         """ Constructor fot he ParametersException object.
 
         Keyword Arguments:
             cause - A string that explains the cause of the exception.
         """
         self.cause = cause
+        self.code = code
 
     def __str__(self):
         return str(self.cause)
