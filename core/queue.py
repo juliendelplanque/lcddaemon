011920ce99ddd0ded9d9c417acf3c43295aa0b1d
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-05-31 20:39:13 +0200

    Refix the pop() method (correctly this time).

--- core/queue.py
+++ core/queue.py
@@ -26,10 +26,11 @@ class MessageQueue(object):
         """
         m = self.queue.get()
         while m.is_outdated():
-            m = self.queue.get()
             users[m.sender] -= 1
+            m = self.queue.get()
+        users[m.sender] -= 1
         if m.repeat > 1:
-            m.repeat = m.repeat - 1
+            m.repeat -= 1
             self.put(m)
         return m
 
