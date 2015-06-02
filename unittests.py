d03381adc2a0d09787ebbdf2649e34937becc441
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-06-02 20:59:08 +0200

    Added unit tests from tests.queuetests module.

--- unittests.py
+++ unittests.py
@@ -6,7 +6,10 @@
 import unittest
 
 from tests.messagetests import MessageTest
+from tests.queuetests import MessageQueueTest
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(MessageTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(MessageQueueTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
