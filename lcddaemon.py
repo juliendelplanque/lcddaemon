c0db435099d79e1ad073a76aad15a8b2f64a2f21
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-06-05 07:48:00 +0200

    Added MessageManager thread.

--- lcddaemon.py
+++ lcddaemon.py
@@ -4,20 +4,29 @@
 """
 
 import sys
+import threading
 
 from core.daemonargs import parse_arguments
 from core.message import set_default_repeat
 from core.message import set_default_ttl
 from core.message import set_default_duration
 from core.queue import MessageQueue
+from core.queuemanager import QueueManager
 from server.server import run
+
+from modules.printer.printer import Printer # To remove, has to be done dynamically
 
 def main():
     config = parse_arguments()
     set_default_repeat(config["ttr"])
     set_default_ttl(config["ttl"])
     set_default_duration(config["ttd"])
-    run(MessageQueue(config["limit"]), config["ptl"])
+    message_queue = MessageQueue(config["limit"])
+    message_manager = QueueManager(message_queue, Printer, None) # TODO (None)
+    message_manager_thread = threading.Thread(target=message_manager.manage)
+    message_manager_thread.daemon = True
+    message_manager_thread.start()
+    run(message_queue, config["ptl"])
 
 if __name__ == '__main__':
     try:
