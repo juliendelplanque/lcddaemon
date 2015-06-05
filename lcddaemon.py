cf6335d1e3613ca9a3adcd0c2e09374df2da8755
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-06-05 19:35:35 +0200

    Updated the script to load dynamically the driver and the animation.

--- lcddaemon.py
+++ lcddaemon.py
@@ -12,20 +12,32 @@ from core.message import set_default_ttl
 from core.message import set_default_duration
 from core.queue import MessageQueue
 from core.queuemanager import QueueManager
-from core.moduleloader import load_module_from_conf
+from core.loader import load_module_from_conf
+from core.loader import load_driver_from_conf
+from core.loader import load_animation_from_conf
 from server.server import run
 
 def main():
+    # Parse args from cmd.
     config = parse_arguments()
+    # Set default values.
     set_default_repeat(config["ttr"])
     set_default_ttl(config["ttl"])
     set_default_duration(config["ttd"])
+    # Create the message queue.
     message_queue = MessageQueue(config["limit"])
+    # Load module, driver and animation according to args.
     module_class = load_module_from_conf(config)
-    message_manager = QueueManager(message_queue, module_class, None) # TODO (None)
+    driver_class = load_driver_from_conf(config)
+    driver = driver_class()
+    animation_class = load_animation_from_conf(config)
+    animation = animation_class(driver)
+    # Create the message manager and start the thread.
+    message_manager = QueueManager(message_queue, module_class, animation)
     message_manager_thread = threading.Thread(target=message_manager.manage)
     message_manager_thread.daemon = True
     message_manager_thread.start()
+    # Start the web server.
     run(message_queue, config["ptl"])
 
 if __name__ == '__main__':
