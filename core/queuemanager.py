a63d0892ee41400e00b93b69145b3ff0b151474c
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-06-16 18:20:26 +0200

    Managed when module, animation or driver create an exception at runtime. See #15.

--- core/queuemanager.py
+++ core/queuemanager.py
@@ -18,8 +18,13 @@ class QueueManager(object):
         """
         while self.keep_going:
             message = self.queue.pop()
-            self.module.apply_actions_with(message)
-            self.animation.animate(message)
+            try:
+                self.module.apply_actions_with(message)
+                self.animation.animate(message)
+            except Exception as e:
+                print("An exception occurs while managing the message: "+str(message))
+                print("Here are some details:")
+                print(str(e))
 
     def stop(self):
         """ Stop the loop that pop messages at next iteration.
