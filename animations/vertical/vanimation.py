60192441188f3f7a4b19a33e8e69c642a37d5a4b
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2015-07-24 10:36:53 +0200

    Implemented vertical animations.

---
+++ animations/vertical/vanimation.py
@@ -0,0 +1,40 @@
+#-*- coding: utf-8 -*-
+import time
+
+from animations.abstractanimation import AbstractAnimation
+from animations.noanimation.noanimation import MultiLineNoAnimation
+
+class VerticalAnimation(AbstractAnimation):
+    def __init__(self, driver):
+        # Call super class constructor.
+        super().__init__(driver)
+        # Re-use an animation already created.
+        self.multi_no_animation = MultiLineNoAnimation(driver)
+
+    def animate(self, message):
+        strings = message.contents.split('\n')
+        if len(strings) > self.driver.line_count():
+            self.display(message, strings)
+        else:
+            self.multi_no_animation.animate(message)
+
+    def display(self, strings):
+        raise NotImplementedError()
+
+class TopToBottomAnimation(VerticalAnimation):
+    def display(self, message, strings):
+        time_per_frame = message.duration/len(strings)
+        for i in range(len(strings)):
+            strings_to_display = strings[i:i+self.driver.line_count()]
+            self.driver.clear()
+            self.driver.write_lines(strings_to_display)
+            time.sleep(time_per_frame)
+
+class BottomToTopAnimation(VerticalAnimation):
+    def display(self, message, strings):
+        time_per_frame = message.duration/len(strings)
+        for i in range(len(strings)):
+            strings_to_display = strings[len(strings)-i-self.driver.line_count():len(strings)-i]
+            self.driver.clear()
+            self.driver.write_lines(strings_to_display)
+            time.sleep(time_per_frame)
